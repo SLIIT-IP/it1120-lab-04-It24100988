48e504e97dcf008729b7323ac66b472310719877
It24100988 <It24100988@my.sliit.lk>
2024-08-14 16:57:56 +0530

    Lab4Q1

---
+++ Q1/IT24100988Lab4Q1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class IT24100988Lab4Q1
+{
+  public static void main(String[]args)
+   
+  {
+        
+	 int number;
+		
+		Scanner input=new Scanner(System.in);
+	    System.out.print("Enter a number - ");
+		number=input.nextInt();
+		
+		if(number > 0)
+		{
+		  System.out.print(" The number is : positive ");
+		}	   
+		else if (number < 0)  
+		{
+			System.out.print(" The number is : Negative ");
+		
+		}
+		else
+		{
+			System.out.print(" The number is : Zero ");
+		}
+		
+			
+		    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+  }
+}
